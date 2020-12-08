eb770515bed82ef7a8da9b872307aa866728bcb4
VINITPANDEY <dev.vinit@outlook.com>
2020-12-08 22:07:14 +0530

    update Readme  File

--- src/conponents/wether.js
+++ src/conponents/wether.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import axios from "axios"
+import axios from 'axios';
 // const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
 
 function Wetherapp() {
     const [wether, setWether] = useState(null);
-
     useEffect(() => {
-        const URL =`api.openweathermap.org/data/2.5/weather?q=patna&appid=d0f85e26df7c7a0773b2b695bed6fc0e`
+        // const URL =`http://api.openweathermap.org/data/2.5/weather?q=patna&appid=d0f85e26df7c7a0773b2b695bed6fc0e`
+        const URL =`http://ursoch.com/api/RestController/user/1`        
         let response = axios.get(URL);
         let data  = response.data; 
         console.log(data)
@@ -20,5 +20,4 @@ function Wetherapp() {
       
     );
   }
-
 export default Wetherapp;
