e75eabc88bfc91446b7d7184c57d3e6cafcbfe22
VINITPANDEY <dev.vinit@outlook.com>
2020-12-11 18:54:02 +0530

    update Readme

--- src/conponents/wether.js
+++ src/conponents/wether.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios"
 // const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
-
 function Wetherapp() {
     const [wether, setWether] = useState([]);
 
@@ -22,14 +21,16 @@ function Wetherapp() {
 
     return (
       <div>
-
-        let city_name="patna";
-        const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=d0f85e26df7c7a0773b2b695bed6fc0e`);
-
+         <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
         <h1>{wether.base}</h1>
         <h1>{wether.name}</h1>
       </div>
-      
     );
   }
 
