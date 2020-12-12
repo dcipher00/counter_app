b75519c5dc5721d1a0f1ecc94bd215f374f45824
VINITPANDEY <dev.vinit@outlook.com>
2020-12-12 10:41:16 +0530

    update wether js file

--- src/conponents/wether.js
+++ src/conponents/wether.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from 'react';
+
 import axios from "axios"
+
 // const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
 function Wetherapp() {
     const [wether, setWether] = useState([]);
+    const [inputval, setInputVal] = useState("");
 
     const getData = async () => {
         let city_name="patna";
         const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=d0f85e26df7c7a0773b2b695bed6fc0e`);
-    //   const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=patna&appid=d0f85e26df7c7a0773b2b695bed6fc0e`);
       return response.data;
     };
+
+    const handleChange=(e)=>{
+        console.log(e.target.value);
+    }
+
+    const handleSubmit=(e)=>{
+        
+    }
 
     useEffect(() => {
       setTimeout(() => {
@@ -21,12 +31,12 @@ function Wetherapp() {
 
     return (
       <div>
-         <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
+         <form onSubmit={handleSubmit}>
+            <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
       </form>
         <h1>{wether.base}</h1>
         <h1>{wether.name}</h1>
